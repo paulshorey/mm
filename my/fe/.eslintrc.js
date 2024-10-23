b9ff9e3b097551e68875273ed2fb0744beef4b1b
Paul Shorey <hello@paulshorey.com>
2024-10-22 22:29:49 -0500

    revert fix config files

--- my/fe/.eslintrc.js
+++ my/fe/.eslintrc.js
@@ -3,4 +3,8 @@ module.exports = {
   root: true,
   extends: ["@my/config/eslint/react-internal.js"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.lint.json",
+    tsconfigRootDir: __dirname,
+  },
 };
