c4311be7c1852006248c2a3e7c4fafd682bcf0b6
Paul Shorey <hello@paulshorey.com>
2024-11-05 07:19:28 -0600

    update fe

--- apps/trade/src/be/dydx/info.ts
+++ apps/trade/src/be/dydx/info.ts
@@ -12,6 +12,7 @@ import {
 } from '@dydxprotocol/v4-client-js'
 import { sendToMyselfSMS } from '@my/be/twillio/sendToMyselfSMS'
 import { logAdd } from '@my/be/sql/log/add'
+import { Order } from '@src/be/dydx/methods/getOrders'
 
 type Output = Record<string, any>
 
@@ -32,28 +33,42 @@ export const dydxScout = async (): Promise<Output | undefined> => {
     const address = wallet.address || ''
     const subaccount = new SubaccountClient(wallet, 0)
     const subaccountNumber = subaccount.subaccountNumber
+    const accountData = (
+      await client.account.getSubaccount(address, subaccountNumber)
+    )?.subaccount
 
     // Fetch data
     const blockHeight = await client.utility.getHeight()
     // const trades = (await client.markets.getPerpetualMarketTrades(ticker))
     //   ?.trades
-    const orders =
+    const orders = (
       (await client.account.getSubaccountOrders(address, subaccountNumber)) ||
       []
-    const positions = (
-      (
-        await client.account.getSubaccountPerpetualPositions(
-          address,
-          subaccountNumber
-        )
-      )?.positions || []
-    ).filter((p: any) => p.status === 'OPEN')
+    ).filter((p: Order) => {
+      return (
+        p.status === 'OPEN' ||
+        p.status === 'UNTRIGGERED' ||
+        p.status === 'PENDING'
+      )
+    })
+    // const positions = (
+    //   (
+    //     await client.account.getSubaccountPerpetualPositions(
+    //       address,
+    //       subaccountNumber
+    //     )
+    //   )?.positions || []
+    // ).filter((p: any) => p.status === 'OPEN')
     // const orderbook = await client.markets.getPerpetualMarketOrderbook(ticker)
     // const asksAndBids = { asks: orderbook.asks, bids: orderbook.bids }
     // const sparklines = await client.markets.getPerpetualMarketSparklines()
 
     // Format data
-    data.positions = positions
+    data.positions = {
+      equity: Number(accountData.equity).toFixed(2),
+      margin: Number(accountData.freeCollateral).toFixed(2),
+      ...accountData.openPerpetualPositions,
+    }
     data.orders = orders
     data.blockHeight = blockHeight
     // data.asksAndBids = asksAndBids
