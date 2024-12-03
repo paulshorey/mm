bae3d0e722a27c61f6f88d09ccc8252cfd283b4f
Paul Shorey <hello@paulshorey.com>
2024-12-03 15:18:29 -0500

    WIP: experimental ordering

--- apps/auth/src/env.ts
+++
@@ -1,9 +0,0 @@
-export const ENV = {
-  PG_HOST: process.env.PG_HOST || '',
-  PG_PORT: Number(process.env.PG_PORT) || 5432,
-  PG_USER: process.env.PG_USER || '',
-  PG_PASSWORD: process.env.PG_PASSWORD || '',
-  PG_DATABASE: process.env.PG_DATABASE || '',
-  //
-  LOG_ENV: process.env.LOG_ENV || 'localhost',
-};
