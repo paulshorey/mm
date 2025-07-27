5a3c461a0e56bc550a50eb8d59e67b29a9d2a973
Paul Shorey <hello@paulshorey.com>
2025-07-27 02:36:54 -0500

    multi filters in url

--- apps/log/src/fe/blocks/FilterBadge.tsx
+++ apps/log/src/fe/blocks/FilterBadge.tsx
@@ -2,6 +2,7 @@
 
 import { Badge } from '@my/fe/src/components/inline/Badge'
 import Link from 'next/link'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 export function FilterBadge({
   field,
@@ -10,15 +11,20 @@ export function FilterBadge({
   field: string
   value: string | boolean
 }) {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
   if (value === null || value === undefined || value === '') {
     return null
   }
+
+  const newParams = new URLSearchParams(searchParams.toString())
+  newParams.set(field, String(value))
 
   const displayValue =
     typeof value === 'boolean' ? (value ? 'dev' : 'pro') : value
   return (
     <Badge className="font-bold pr-2">
-      <Link key="edit" href={`/?${field}=${value}`}>
+      <Link key="edit" href={`${pathname}?${newParams.toString()}`}>
         {' '}
         {displayValue}{' '}
       </Link>
--- apps/log/src/fe/blocks/FilterBadgeTime.tsx
+++ apps/log/src/fe/blocks/FilterBadgeTime.tsx
@@ -4,8 +4,11 @@ import { Badge } from '@my/fe/src/components/inline/Badge'
 import Link from 'next/link'
 import { getDayRange } from '@my/fe/src/lib/time'
 import { LocalShortTime } from '@my/fe/src/components/inline/LocalShortTime'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 export function FilterBadgeTime({ time }: { time: number }) {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
   if (!time) {
     return null
   }
@@ -13,10 +16,14 @@ export function FilterBadgeTime({ time }: { time: number }) {
   const { startOfDay, endOfDay } = getDayRange(time)
   const [localTime, localDate] = LocalShortTime({ epoch: time })
 
+  const newParams = new URLSearchParams(searchParams.toString())
+  newParams.set('time_start', String(startOfDay))
+  newParams.set('time_end', String(endOfDay))
+
   return (
     <Badge className="font-bold pr-2">
       <span className="mr-1">{localTime}</span>
-      <Link key="edit" href={`/?time_start=${startOfDay}&time_end=${endOfDay}`}>
+      <Link key="edit" href={`${pathname}?${newParams.toString()}`}>
         {localDate}
       </Link>
     </Badge>
