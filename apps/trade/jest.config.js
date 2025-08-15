b74ac637ba263d936b5cdf34bb25ffea74293dca
Paul Shorey <hello@paulshorey.com>
2025-08-15 18:36:55 -0500

    fix tests

--- apps/trade/jest.config.js
+++ apps/trade/jest.config.js
@@ -3,8 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-import { type Config } from 'jest'
-import nextJest from 'next/jest.js'
+const nextJest = require('next/jest.js')
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -21,11 +20,10 @@ const config = {
     '^@src/(.*)$': '<rootDir>/src/$1',
     '^@apps/common/(.*)$': '<rootDir>/../common/$1',
   },
-  preset: 'ts-jest',
   testEnvironmentOptions: {
     customExportConditions: [''],
   },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config) as () => Promise<Config>
+module.exports = createJestConfig(config)
