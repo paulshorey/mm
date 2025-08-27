d60e30c0ac4ceeb8e9a1adfa15a9bb13215ab490
Paul Shorey <hello@paulshorey.com>
2025-08-27 11:38:40 -0500

    debug logs console log every log

--- apps/common/sql/strength/types.ts
+++ apps/common/sql/strength/types.ts
@@ -4,55 +4,21 @@ export type StrengthRowGet = {
   timenow: Date; // DateTime as ISO string
   price: number;
   volume: number;
-  "30S": number | null;
-  "1": number | null;
-  "2": number | null;
-  "3": number | null;
-  "4": number | null;
-  "5": number | null;
-  "6": number | null;
-  "7": number | null;
-  "9": number | null;
-  "11": number | null;
-  "13": number | null;
-  "12": number | null;
-  "24": number | null;
-  "48": number | null;
-  "60": number | null;
-  "72": number | null;
-  "90": number | null;
   server_name: string;
   app_name: string;
   node_env: string;
   created_at: Date; // DateTime as ISO string
-};
+} & Record<string, number | null>;
 export type StrengthRowAdd = {
   ticker: string;
   timenow: Date; // DateTime as ISO string
   price: number | null;
   volume: number | null;
-  "30S"?: number | null;
-  "1"?: number | null;
-  "2"?: number | null;
-  "3"?: number | null;
-  "4"?: number | null;
-  "5"?: number | null;
-  "6"?: number | null;
-  "7"?: number | null;
-  "9"?: number | null;
-  "11"?: number | null;
-  "13"?: number | null;
-  "12"?: number | null;
-  "24"?: number | null;
-  "48"?: number | null;
-  "60"?: number | null;
-  "72"?: number | null;
-  "90"?: number | null;
   server_name?: string;
   app_name?: string;
   node_env?: string;
   created_at?: Date; // DateTime as ISO string
-};
+} & Record<string, number | null>;
 
 export type StrengthDataAdd = {
   ticker: string | null;
